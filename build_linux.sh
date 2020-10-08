61e35f5017181ad5d1ab0429395fc180f788ef1c
Nikita Prokopov <niki@tonsky.me>
2020-10-08 21:17:17 +0200

    Trying creating release & uploading

--- build_linux.sh
+++ build_linux.sh
@@ -4,45 +4,49 @@ cd "`dirname $0`"
 
 source ./checkout.sh
 
-python2 tools/git-sync-deps
-gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false \
-  skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false \
-  skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true \
-  skia_use_system_freetype2=false skia_use_system_harfbuzz=false skia_enable_gpu=true skia_use_gl=true \
-  target_cpu=\"x64\" extra_cflags_cc=[\"-frtti\"] cxx=\"g++-9\""
-ninja -C out/Release-x64 skia modules
+# python2 tools/git-sync-deps
+# gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false \
+#   skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false \
+#   skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true \
+#   skia_use_system_freetype2=false skia_use_system_harfbuzz=false skia_enable_gpu=true skia_use_gl=true \
+#   target_cpu=\"x64\" extra_cflags_cc=[\"-frtti\"] cxx=\"g++-9\""
+# ninja -C out/Release-x64 skia modules
 
-zip --recurse-paths --quiet ../Skia-${release}-linux-Release-x64.zip \
-  out/Release-x64/*.a \
-  include \
-  modules/particles/include/*.h \
-  modules/skottie/include/*.h \
-  modules/skparagraph/include/*.h \
-  modules/skplaintexteditor/include/*.h \
-  modules/skresources/include/*.h \
-  modules/sksg/include/*.h \
-  modules/skshaper/include/*.h \
-  src/core/*.h \
-  src/gpu/gl/*.h \
-  third_party/externals/angle2/LICENSE \
-  third_party/externals/angle2/include \
-  third_party/externals/freetype/docs/FTL.TXT \
-  third_party/externals/freetype/docs/GPLv2.TXT \
-  third_party/externals/freetype/docs/LICENSE.TXT \
-  third_party/externals/freetype/include \
-  third_party/externals/libpng/LICENSE \
-  third_party/externals/libpng/*.h \
-  third_party/externals/libwebp/COPYING \
-  third_party/externals/libwebp/PATENTS \
-  third_party/externals/libwebp/src/dec/*.h \
-  third_party/externals/libwebp/src/dsp/*.h \
-  third_party/externals/libwebp/src/enc/*.h \
-  third_party/externals/libwebp/src/mux/*.h \
-  third_party/externals/libwebp/src/utils/*.h \
-  third_party/externals/libwebp/src/webp/*.h \
-  third_party/externals/harfbuzz/COPYING \
-  third_party/externals/harfbuzz/src/*.h \
-  third_party/externals/swiftshader/LICENSE.txt \
-  third_party/externals/swiftshader/include \
-  third_party/externals/zlib/LICENSE \
-  third_party/externals/zlib/*.h
+# zip --recurse-paths --quiet ../Skia-${release}-linux-Release-x64.zip \
+#   out/Release-x64/*.a \
+#   include \
+#   modules/particles/include/*.h \
+#   modules/skottie/include/*.h \
+#   modules/skparagraph/include/*.h \
+#   modules/skplaintexteditor/include/*.h \
+#   modules/skresources/include/*.h \
+#   modules/sksg/include/*.h \
+#   modules/skshaper/include/*.h \
+#   src/core/*.h \
+#   src/gpu/gl/*.h \
+#   third_party/externals/angle2/LICENSE \
+#   third_party/externals/angle2/include \
+#   third_party/externals/freetype/docs/FTL.TXT \
+#   third_party/externals/freetype/docs/GPLv2.TXT \
+#   third_party/externals/freetype/docs/LICENSE.TXT \
+#   third_party/externals/freetype/include \
+#   third_party/externals/libpng/LICENSE \
+#   third_party/externals/libpng/*.h \
+#   third_party/externals/libwebp/COPYING \
+#   third_party/externals/libwebp/PATENTS \
+#   third_party/externals/libwebp/src/dec/*.h \
+#   third_party/externals/libwebp/src/dsp/*.h \
+#   third_party/externals/libwebp/src/enc/*.h \
+#   third_party/externals/libwebp/src/mux/*.h \
+#   third_party/externals/libwebp/src/utils/*.h \
+#   third_party/externals/libwebp/src/webp/*.h \
+#   third_party/externals/harfbuzz/COPYING \
+#   third_party/externals/harfbuzz/src/*.h \
+#   third_party/externals/swiftshader/LICENSE.txt \
+#   third_party/externals/swiftshader/include \
+#   third_party/externals/zlib/LICENSE \
+#   third_party/externals/zlib/*.h
+
+popd
+pushd depot_tools
+zip --recurse-paths --quiet ../depot-linux.zip *
---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+cd "`dirname $0`"
+
+auth="Authorization: token ${GITHUB_TOKEN}"
+accept="Accept: application/vnd.github.v3+json"
+
+if ! curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" https://api.github.com/repos/tonsky/skia-build/releases/tags/${release} > release.json ; then
+  echo "> Creating release ${release}"
+  curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" --request POST \
+    --data "{\"tag_name\":\"${release}\"}" \
+    https://api.github.com/repos/tonsky/skia-build/releases > release.json
+else
+  echo "> Release ${release} exists"
+fi
+
+archive=`ls *.zip`
+[[ $(cat release.json | grep '"upload_url"') =~ https://.*/assets ]]
+upload_url="${BASH_REMATCH[0]}?name=${archive}"
+rm release.json
+
+echo "Uploading ${archive} to ${upload_url}"
+curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" --header "Content-Type: application/zip" --request POST --data-binary "@${archive}" ${upload_url}
