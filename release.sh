e16e7bc6aa2b653df174217d46e9c2b43eb9d409
Nikita Prokopov <niki@tonsky.me>
2020-10-29 23:07:43 +0100

    check_release, restored release for win

--- release.sh
+++ release.sh
@@ -8,18 +8,17 @@ accept="Accept: application/vnd.github.v3+json"
 if ! curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" https://api.github.com/repos/JetBrains/skia-build/releases/tags/${release} > release.json ; then
   echo "> Creating release ${release}"
   curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" --request POST \
-    --data "{\"tag_name\":\"${release}\"}" \
+    --data "{\"tag_name\":\"${release}\",\"name\":\"${release}\"}" \
     https://api.github.com/repos/JetBrains/skia-build/releases > release.json
 else
   echo "> Release ${release} exists"
+  cat release.json
 fi
 
-for archive in *.zip; do 
-  [[ $(cat release.json | grep '"upload_url"') =~ https://.*/assets ]]
-  upload_url="${BASH_REMATCH[0]}?name=${archive}"
-
-  echo "Uploading ${archive} to ${upload_url}"
-  curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" --header "Content-Type: application/zip" --request POST --data-binary "@${archive}" ${upload_url}
-done
-
+archive=`ls *.zip`
+[[ $(cat release.json | grep '"upload_url"') =~ https://.*/assets ]]
+upload_url="${BASH_REMATCH[0]}?name=${archive}"
 rm release.json
+
+echo "Uploading ${archive} to ${upload_url}"
+curl --fail --location --silent --show-error --header "${auth}" --header "${accept}" --header "Content-Type: application/zip" --request POST --data-binary "@${archive}" ${upload_url}
