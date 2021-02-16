8a4724467510fa22f253f92277040d6f4e453d43
Roman Sedaikin <rsedaikin@gmail.com>
2021-02-16 16:41:59 +0300

    Unlocked DirectX support.

--- script/build.py
+++ script/build.py
@@ -53,7 +53,7 @@ def main():
   elif 'windows' == common.system:
     args += [
       # 'skia_use_angle=true',
-      # 'skia_use_direct3d=true',
+      'skia_use_direct3d=true',
       'extra_cflags=["-DSK_FONT_HOST_USE_SYSTEM_SETTINGS"]',
     ]
 
